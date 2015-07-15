74d27a0e80ab0052d1a953be8882510a7e26a217
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-07-15 00:52:55 -0600

    Better comments and the start of the next fn

--- src/simplesearch/SimpleSearch.hx
+++ src/simplesearch/SimpleSearch.hx
@@ -4,16 +4,18 @@ using thx.Strings;
 
 class SimpleSearch {
   /**
-   * Compare a search string against a single target word.
-   */
-  private function matchSingleWord(word : String, search : String) : Bool {
+   * Compare a search string against a single target word. The search string can
+   * have gaps, but its characters must match the order in the `word`. For
+   * example, search "fbr" matches word "foobar", but search "brf" does not.
+   **/
+  private static function matchSingleWord(word : String, search : String) : Bool {
     var prevMatch = -1; // location to start searching for matching chars
 
     // loop through each letter in `search`m ensuring it matches
     for (i in search.length) {
       var currentChar = search.charAt(i).toLowerCase();
 
-      // non-alphanumeric characters can't cause a failed match
+      // non-alphanumeric search characters can't cause a failed match
       if (!currentChar.isAlphaNum()) continue;
 
       // return false if the current character of `search`
@@ -29,6 +31,20 @@ class SimpleSearch {
     }
 
     // made it, so the search matches the target word
+    return true;
+  }
+
+  /**
+   * Compare a search string against multiple target words, ignoring word order.
+   * Will return true only if all chars in the search string are found in the
+   * `words`, following rules for individual word matches above.
+   **/
+  private static function matchMultipleWords(words : Array<String>, search : String) {
+    // loop through each character in the search string, finding all words
+    // that match the entire subset of the search string up to this point
+    for (i in search.length) {
+
+    }
     return true;
   }
 }
